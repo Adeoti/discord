39133f8c4df82c49cb33640777d91c9d95564239
Adeoti <adeoti360@gmail.com>
2023-03-14 07:49:23 +0100

    Framer-motion altered

--- src/components/Belong.jsx
+++ src/components/Belong.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import BelongImg from '../images/belongimg.svg';
-import {motion as m} from 'framer-motion';
+import {motion} from 'framer-motion';
 const Belong = () => {
   return (
     <div className='belong'>
         <div className='bridge'>
-            <m.div className='belong__grid'
+            <motion.div className='belong__grid'
             initial={{scale:0.5, opacity:0, rotate:-90,}}
             whileInView={{scale:1, opacity:1, rotate:0,}}
             transition={{duration:1}}
@@ -21,7 +21,7 @@ const Belong = () => {
                     Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.
                     </p>
                    </div>
-            </m.div>
+            </motion.div>
         </div>
     </div>
   )
